34ca6b6d94fa9d21047029f2f1e615051c62f997
Apple <apple@apples-macbook-pro-50.local>
2019-06-20 13:01:36 -0700

    Complete Iteration 1

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -14,7 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func showMessage (sender: UIButton) {
+        let alertController = UIAlertController(title: "It is okay to be dumb", message: "Success usually comes to those who are too busy to be looking for it.", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+        
+        //in as much detail as possible
+    //pseudo-code the thing that need to happen
+    // when a user clicks an emoji button
 }
-
+}
